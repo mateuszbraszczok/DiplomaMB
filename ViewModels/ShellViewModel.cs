cd9ddacc3394f45ff3dfdb073a3e5e8fca5d72ca
Mateusz Braszczok <mateusz.braszczok@embevity.com>
2023-08-10 22:56:08 +0200

    added derivative calc

--- ViewModels/ShellViewModel.cs
+++ ViewModels/ShellViewModel.cs
@@ -56,6 +56,7 @@ namespace DiplomaMB.ViewModels
         {
             NotifyOfPropertyChange(() => CanSpectrumOperations);
             NotifyOfPropertyChange(() => CanSpectrumPeaks);
+            NotifyOfPropertyChange(() => CanEditSmoothing);
         }
 
         private int frames_to_acquire;
@@ -445,6 +446,10 @@ namespace DiplomaMB.ViewModels
         }
 
 
+        public bool CanEditSmoothing
+        {
+            get { return Spectrums?.Count > 0; }
+        }
         public void EditSmoothing()
         {
             if (selected_spectrum == null)
@@ -482,6 +487,17 @@ namespace DiplomaMB.ViewModels
                     UpdatePlot();
                 }
             }
+        }
+
+        public void Derivative()
+        {
+
+            Spectrum result = Spectrometer.CalculateDerivative(1, 3, SelectedSpectrum);
+
+            result.Id = last_id++;
+
+            Spectrums.Add(result);
+            UpdatePlot();
         }
 
         private bool IsSpectrometerConnected()
